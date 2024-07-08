fa471c23f7b79fa62fcdaf25ee5a9e3a7a6e7982
ThaVip <138695566+ThaVip@users.noreply.github.com>
2024-07-08 14:25:33 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,7 +7,5 @@ class User(db.Model):
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150), nullable=False)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
